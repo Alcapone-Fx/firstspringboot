9acead1c81422da358581504ef1a065eb4128d5a
Felix Navas <navasfx19@gmail.com>
2023-12-04 21:49:33 -0600

    :tada: New Spring Boot Project

---
+++ src/test/java/com/first/springboot/firstspringboot/FirstspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.springboot.firstspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
