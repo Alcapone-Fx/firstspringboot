d61091f3617618cdb550179731bab256f4ddf009
Felix Navas <navasfx19@gmail.com>
2023-12-05 22:40:38 -0600

    :sparkles: Get custom properties

--- src/main/java/com/first/springboot/firstspringboot/rest/FunRestController.java
+++ src/main/java/com/first/springboot/firstspringboot/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.first.springboot.firstspringboot.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${user.other}")
+    private String userName;
+
+    @Value("${user.age}")
+    private String userAge;
+
     // expose "/" that rerturn "Hello World"
     @GetMapping("/")
     public String sayHello() {
@@ -14,5 +21,10 @@ public class FunRestController {
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run, run and run";
+    }
+
+    @GetMapping("/props")
+    public String getProps(){
+        return this.userName + this.userAge;
     }
 }
